d06ee9924df40f9130e63e52c97e36ec0e8eba83
Ariful Islam <remalarif0130@gmail.com>
2024-05-14 19:39:20 +0600

    started

---
+++ index.js
@@ -0,0 +1,9 @@
+// local module
+// const {a,add} = require('./local-1')
+// const {a:a2,add:add2} = require('./local-2')
+
+// // console.log(ar.add(2,3))
+// console.log(add2(90,6,2))
+
+// built in module
+
